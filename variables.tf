1689c218e0ad845625daf4fb519c29382e02c2a0
pushpahari <chetalapushpa@gmail.com>
2024-09-07 22:19:53 +0100

    files added

--- variables.tf
+++ variables.tf
@@ -5,8 +5,9 @@ variable "vpc_cidr_block" {
   default     = "10.0.0.0/16"
 }
 
-variable "public_cidir_blocks" {
-  description = "public cidir blocks"
-  type = list(string)
-  default = ["10.0.1.0/24","10.0.2.0/24"]
+variable "public_cidr_blocks" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
+
